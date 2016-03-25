533a7f79d79ded48964312492a99b7c270c5791e
chrisjmccormick <chrisjmccormick@gmail.com>
2016-03-25 14:18:16 -0700

    Implemented tfidf / LSA classification

--- runClassification_LSA_L2.py
+++ runClassification_LSA_L2.py
@@ -12,94 +12,124 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
+from sklearn.neighbors import KNeighborsClassifier
 
 
-
-#==============================================================================
-#  Load the dataset.
+###############################################################################
+#  Load the raw text dataset.
+###############################################################################
 
 print("Loading dataset...")
 
-# Load the dataset (with pre-computed fingerprints).
-X_train = pickle.load( open( "X_train.p", "rb" ) )
-y_train = pickle.load( open( "y_train.p", "rb" ) ) 
+# The raw text dataset is stored as tuple in the form:
+# (X_train_raw, y_train_raw, X_test_raw, y_test)
+# The 'filtered' dataset excludes any articles that we failed to retrieve
+# fingerprints for.
+raw_text_dataset = pickle.load( open( "data/raw_text_dataset_filtered.pickle", "rb" ) )
+X_train_raw = raw_text_dataset[0]
+y_train = raw_text_dataset[1] 
+X_test_raw = raw_text_dataset[2]
+y_test = raw_text_dataset[3]
 
-X_test = pickle.load( open( "X_test.p", "rb" ) )
-y_test = pickle.load( open( "y_test.p", "rb" ) )
 
-print("%d training examples (%d positive)" % (len(y_train), sum(y_train)))
-print("%d test examples (%d positive)" % (len(y_test), sum(y_test)))
+print("  %d training examples (%d positive)" % (len(y_train), sum(y_train)))
+print("  %d test examples (%d positive)" % (len(y_test), sum(y_test)))
 
-#==============================================================================
-#  
 
 ###############################################################################
 #  Use LSA to vectorize the articles.
 ###############################################################################
 
+# Tfidf vectorizer:
+#   - Strips out “stop words”
+#   - Filters out terms that occur in more than half of the docs (max_df=0.5)
+#   - Filters out terms that occur in only one document (min_df=2).
+#   - Selects the 10,000 most frequently occuring words in the corpus.
+#   - Normalizes the vector (L2 norm of 1.0) to normalize the effect of 
+#     document length on the tf-idf values. 
 vectorizer = TfidfVectorizer(max_df=0.5, max_features=10000,
                              min_df=2, stop_words='english',
                              use_idf=True)
 
+# Build the tfidf vectorizer from the training data ("fit"), and apply it 
+# ("transform").
 X_train_tfidf = vectorizer.fit_transform(X_train_raw)
-X_test_tfidf = vectorizer.fit_transform(X_test_raw)
 
-print("Performing dimensionality reduction using LSA")
+print("\nPerforming dimensionality reduction using LSA")
 t0 = time.time()
-# Vectorizer results are normalized, which makes KMeans behave as
-# spherical k-means for better results. Since LSA/SVD results are
-# not normalized, we have to redo the normalization.
-svd = TruncatedSVD(256)
+
+# Project the tfidf vectors onto the first 150 principal components.
+# Though this is significantly fewer features than the original tfidf vector,
+# they are stronger features, and the accuracy is higher.
+svd = TruncatedSVD(100)
 lsa = make_pipeline(svd, Normalizer(copy=False))
 
-X_train_LSA = lsa.fit_transform(X_train_raw)
-X_test_LSA = lsa.fit_transform(X_test_raw)
+# Run SVD on the training data, then project the training data.
+X_train_lsa = lsa.fit_transform(X_train_tfidf)
 
-print("done in %fs" % (time.time() - t0))
+print("  done in %.3fsec" % (time.time() - t0))
 
 explained_variance = svd.explained_variance_ratio_.sum()
-print("Explained variance of the SVD step: {}%".format(int(explained_variance * 100)))
-print()
+print("  Explained variance of the SVD step: {}%".format(int(explained_variance * 100)))
 
 
+# Now apply the transformations to the test data as well.
+X_test_tfidf = vectorizer.transform(X_test_raw)
+X_test_lsa = lsa.transform(X_test_tfidf)
+
+
+###############################################################################
+#  Run classification of the test articles
+###############################################################################
+
+print("\nClassifying tfidf vectors...")
 
 # Time this step.
 t0 = time.time()
 
+# Build a k-NN classifier. Use k = 5 (majority wins), the cosine distance, 
+# and brute-force calculation of distances.
+knn_tfidf = KNeighborsClassifier(n_neighbors=5, algorithm='brute', metric='cosine')
+knn_tfidf.fit(X_train_tfidf, y_train)
+
+# Classify the test vectors.
+p = knn_tfidf.predict(X_test_tfidf)
+
+# Measure accuracy
 numRight = 0;
+for i in range(0,len(p)):
+    if p[i] == y_test[i]:
+        numRight += 1
 
-curTestIndex = 0;
-
-# Classify each of the test vectors using the nearest neighbor.
-for x1 in X_test:
-    x1 = set(x1)
-
-    print("%d / %d" % (curTestIndex, len(y_test)))
-
-    # Create an array to hold the overlap.
-    Overlap = []
-
-    # Compare it to each of the training vectors.
-    for x2 in X_train:
-        x2 = set(x2)
-        
-        # Calculate the overlap and add it to the list.    
-        Overlap.append(len(x1 & x2))
-    
-    # Get the index of the document with the most overlap.
-    closestIndex = Overlap.index(max(Overlap))
-    
-    # Check if the test example was correctly classified.
-    if (y_test[curTestIndex] == y_train[closestIndex]):
-        numRight = numRight + 1
-    
-    curTestIndex = curTestIndex + 1
-
-    #print("Closest matching document %d, JS %.2f, cat: %d" %(closestIndex, JS[closestIndex], y_train[closestIndex]) )
-
-print("(%d / %d) correct - %.2f%%" % (numRight, len(y_test), float(numRight) / float(len(y_test)) * 100.0))
+print("  (%d / %d) correct - %.2f%%" % (numRight, len(y_test), float(numRight) / float(len(y_test)) * 100.0))
 
 # Calculate the elapsed time (in seconds)
 elapsed = (time.time() - t0)
-        
-print "\nClassification took %.2fsec" % elapsed
+print("  done in %.3fsec" % elapsed)
+
+
+print("\nClassifying LSA vectors...")
+
+# Time this step.
+t0 = time.time()
+
+# Build a k-NN classifier. Use k = 5 (majority wins), the cosine distance, 
+# and brute-force calculation of distances.
+knn_lsa = KNeighborsClassifier(n_neighbors=5, algorithm='brute', metric='cosine')
+knn_lsa.fit(X_train_lsa, y_train)
+
+# Classify the test vectors.
+p = knn_lsa.predict(X_test_lsa)
+
+# Measure accuracy
+numRight = 0;
+for i in range(0,len(p)):
+    if p[i] == y_test[i]:
+        numRight += 1
+
+print("  (%d / %d) correct - %.2f%%" % (numRight, len(y_test), float(numRight) / float(len(y_test)) * 100.0))
+
+# Calculate the elapsed time (in seconds)
+elapsed = (time.time() - t0)    
+print("    done in %.3fsec" % elapsed)
+
